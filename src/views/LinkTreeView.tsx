96d03bc0a0f671424914cba3f2eaaa77d745147c
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-01-02 18:10:33 -0600

    social networks added

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { social } from '../data/social'
+import DevTreeInput from '../components/DevTreeInput'
 
 export default function LinkTreeView() {
+
+  const [devTreeLinks, setDevTreeLinks] = useState(social)
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //console.log(e.target.value);
+    const updatedLinks = devTreeLinks.map(link => link.name == e.target.name ? {...link, url: e.target.value } : link)
+    //console.log(updatedLinks)
+    setDevTreeLinks(updatedLinks)
+    
+    
+  }
+
   return (
-    <div>
-      LinkTreeView
-    </div>
+    <>
+      <div className="space-y-5">
+        {devTreeLinks.map( item => (
+          <DevTreeInput 
+            key={ item.name }
+            item={item}
+            handleUrlChange={handleUrlChange}
+          />
+        ))}
+      </div>
+    </>
   )
 }
